72338c318c4742121f098b3abb73ed7ef3a7bfee
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2022-10-11 23:30:01 +0600

    components folder created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home';
 
 function App() {
   const router = createBrowserRouter([
-
+    {
+      path: '/',
+      element: <Home></Home>,
+    }
   ])
   return (
     <div className="App">
